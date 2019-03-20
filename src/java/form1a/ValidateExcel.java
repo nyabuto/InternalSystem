138a11fd291a36e63bd7ebaa86cb56c55baa97ca
Geofrey Nyabuto <mwambage@gmail.com>
2019-03-20 14:58:39 +0300

    updated excel and excel output

--- src/java/form1a/ValidateExcel.java
+++ src/java/form1a/ValidateExcel.java
@@ -8,6 +8,19 @@ package form1a;
 import database.dbConn;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.FontFamily;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -15,64 +28,102 @@ import java.sql.SQLException;
  */
 public class ValidateExcel {
     dbConn conn = new dbConn();
+    int error_count,warning_count; 
    
-    public String validate(String facility_id,String yearmonth) throws SQLException{
-        int error_counter=0;
-        String errors="";
-        String lhs,rhs,sign,query;
-        String getRules="SELECT codes,validation,iscritical,message,fine_age FROM fas_validation WHERE active=1 AND source='excel'";
+    public JSONObject validate(String[] facilityids, String period[]) throws SQLException{
+        JSONObject obj = new JSONObject();
+        JSONArray jarray = new JSONArray();
+        String facility_id,yearmonth;
+        
+        for(int i=0;i<facilityids.length;i++){
+          for( int j=0;j<period.length;j++){
+          facility_id = facilityids[i];
+          yearmonth = period[j];   
+          JSONObject obj_item = validate(facility_id,yearmonth);
+          jarray.add(obj_item);
+              System.out.println("facility :"+i+" month : "+j);
+          }
+          }
+      obj.put("output", jarray);
+        return obj;
+    }
+    
+    public JSONObject validate(String facility_id,String yearmonth) throws SQLException{
+           JSONArray jarray = new JSONArray();
+           
+        int error_counter=0,warnings_counter=0;
+        String message_type="",message="";
+        String lhs,rhs,sign,query="";
+        String getRules="SELECT section_name,codes,validation,iscritical,message,fine_age,raw_query FROM fas_validation WHERE active=1 AND source='excel' AND codes!='General'";
+        //System.out.println("rules :"+getRules);
         conn.rs = conn.st.executeQuery(getRules);
         while(conn.rs.next()){
-            if(conn.rs.getInt("iscritical")==1){
             //error indicators
              String[] Rules = extract_rule(conn.rs.getString("validation"));  
              lhs = Rules[0];
              rhs = Rules[1];
              sign = Rules[2];
+             
+             if(conn.rs.getString("raw_query")!=null && conn.rs.getString("raw_query").contains("SELECT")){ // for raw queries
+             query = conn.rs.getString("raw_query"); 
+             query = query.replace("YMONTH", yearmonth);
+             query = query.replace("FID", facility_id);
+             }
+             else{
             query = getQuery(conn.rs.getInt("fine_age"));
              
             query = query.replace("lhs", lhs);
             query = query.replace("rhs", rhs);
             query = query.replace("sign", sign);
-            System.out.println("final query"+query);
+            
+            query += "  WHERE yearmonth="+yearmonth+" AND facility_id="+facility_id;  
+             }
                 //run the validation query
                 
                 conn.rs1 = conn.st1.executeQuery(query);
                 ResultSetMetaData metaData = conn.rs1.getMetaData();
                 int column_count = metaData.getColumnCount(); //number of column
-                if(conn.rs.next()){
+                if(conn.rs1.next()){
                    // check and see the columns that have an issue 
                    int cols=1;
                    while(cols<=column_count) {
-                    if(conn.rs.getInt(cols)==1) {
+                          JSONObject objerr = new JSONObject();
+                    if(conn.rs1.getInt(cols)==1) {
+                        //System.out.println(""+query);
                         //archive this error
-                       
+                         if(conn.rs.getInt("iscritical")==1){
+                        message_type = "error";
                         error_counter++;
-                        
-                        errors+=error_counter+". "+conn.rs.getString("message")+" for "+metaData.getColumnLabel(cols)+" i.e "+conn.rs.getString("codes")+"<br>";
-                         
+                        }
+                        else{
+                          message_type = "warning";
+                          warnings_counter++;
+                       }
+                         message = conn.rs.getString("message")+" i.e ["+conn.rs.getString("codes")+"]";
+                         //add error to object
+                
+                   objerr.put("message_type", message_type);
+                   objerr.put("message", message);
+                   objerr.put("program", conn.rs.getString("section_name"));
+                   objerr.put("age_group", metaData.getColumnLabel(cols));   
+                   jarray.add(objerr);
                     }  
                        
                        cols++;
                    }
                     
                 }
-                
-            }
-            else{
-               //warning indicators 
-                
-            }
+           
         }
         
-        return errors;
+        return getFacilityBasics(facility_id,yearmonth,error_counter,warnings_counter,jarray);
     }
     
     private String[] extract_rule(String rule){
       String sign="";
-      if(rule.contains("<=")){sign="<=";}
-      else if(rule.contains("<=")){sign="<=";}
+           if(rule.contains("<=")){sign="<=";}
       else if(rule.contains(">=")){sign=">=";}
+      else if(rule.contains("!=")){sign="!=";}
       else if(rule.contains("=")){sign="=";}
       else if(rule.contains("<")){sign="<";}
       else if(rule.contains(">")){sign=">";}
@@ -81,8 +132,6 @@ public class ValidateExcel {
       return rule.split("#");
         
     }
-    
-    
     private String getQuery(int fine_age){
         String query = "";
        
@@ -125,7 +174,7 @@ public class ValidateExcel {
                 //only for under 1's
                 query="SELECT\n" +
                         "(SUM(IFNULL(CASE WHEN indicator_id IN(lhs) THEN f_1 END,0)) sign SUM(IFNULL(CASE WHEN  indicator_id IN(rhs) THEN f_1 end,0))) AS '<1 F',\n" +
-                        "(SUM(IFNULL(CASE WHEN indicator_id IN(lhs) THEN m_1 END,0)) sign SUM(IFNULL(CASE WHEN  indicator_id IN(rhs) THEN m_1 end,0))) AS '<1 M',\n" +
+                        "(SUM(IFNULL(CASE WHEN indicator_id IN(lhs) THEN m_1 END,0)) sign SUM(IFNULL(CASE WHEN  indicator_id IN(rhs) THEN m_1 end,0))) AS '<1 M'\n" +
                         "FROM fas_temp ";
                 break;
             default:
@@ -134,4 +183,191 @@ public class ValidateExcel {
     
     return query;
     }
+    private JSONObject getFacilityBasics(String facilityID,String yearmonth,int errors,int warnings,JSONArray jarray) throws SQLException{
+      JSONObject obj = new JSONObject();
+      
+      String query = 
+        "SELECT county.County AS County, district.DistrictNom AS SubCounty, subpartnera.SubPartnerNom AS HealthFacility, subpartnera.CentreSanteId AS MFLCode,\n" +
+        "substr('"+yearmonth+"',1,4) AS Year,monthname(str_to_date(substr('"+yearmonth+"',5,6),'%m')) as Month \n" +
+        "FROM subpartnera LEFT JOIN district ON subpartnera.DistrictID=district.DistrictID LEFT JOIN county ON district.CountyID=county.CountyID " +
+        "WHERE SubPartnerID="+facilityID;
+      conn.rs = conn.st.executeQuery(query);
+      ResultSetMetaData metaData = conn.rs.getMetaData();
+      int column_count = metaData.getColumnCount(); //number of column
+     if(conn.rs.next()){
+        
+         for(int i=1; i<=column_count;i++){
+           obj.put(metaData.getColumnLabel(i), conn.rs.getString(i));
+         }
+     } 
+      
+     obj.put("yearmonth", yearmonth);
+     obj.put("errors", errors);
+     obj.put("warnings", warnings);
+     obj.put("details", jarray);
+     return obj;
+    }
+    
+    public XSSFWorkbook generateExcel(JSONObject obj){
+        XSSFWorkbook wb = new XSSFWorkbook();
+        XSSFSheet sheetErrors = wb.createSheet("Errors");
+        XSSFSheet sheetWarning = wb.createSheet("Early Warning");
+  
+       // 
+    XSSFCellStyle styleHeader = wb.createCellStyle();
+    styleHeader.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
+    styleHeader.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+    styleHeader.setBorderTop(BorderStyle.THIN);
+    styleHeader.setBorderBottom(BorderStyle.THIN);
+    styleHeader.setBorderLeft(BorderStyle.THIN);
+    styleHeader.setBorderRight(BorderStyle.THIN);
+    styleHeader.setAlignment(HorizontalAlignment.CENTER);
+    
+    XSSFFont fontHeader = wb.createFont();
+    fontHeader.setColor(HSSFColor.BLACK.index);
+    fontHeader.setBold(true);
+    fontHeader.setFamily(FontFamily.MODERN);
+    fontHeader.setFontName("Cambria");
+    fontHeader.setFontHeight(15);
+    styleHeader.setFont(fontHeader);
+    styleHeader.setWrapText(true);
+    
+    XSSFCellStyle stylex = wb.createCellStyle();
+    stylex.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
+    stylex.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+    stylex.setBorderTop(BorderStyle.THIN);
+    stylex.setBorderBottom(BorderStyle.THIN);
+    stylex.setBorderLeft(BorderStyle.THIN);
+    stylex.setBorderRight(BorderStyle.THIN);
+    stylex.setAlignment(HorizontalAlignment.LEFT);
+    
+    XSSFFont fontx = wb.createFont();
+    fontx.setColor(HSSFColor.BLACK.index);
+    fontx.setBold(true);
+    fontx.setFamily(FontFamily.MODERN);
+    fontx.setFontName("Cambria");
+    stylex.setFont(fontx);
+    stylex.setWrapText(true);
+    
+    XSSFCellStyle stborder = wb.createCellStyle();
+    stborder.setBorderTop(BorderStyle.THIN);
+    stborder.setBorderBottom(BorderStyle.THIN);
+    stborder.setBorderLeft(BorderStyle.THIN);
+    stborder.setBorderRight(BorderStyle.THIN);
+    stborder.setAlignment(HorizontalAlignment.LEFT);
+    
+    XSSFFont font_cell=wb.createFont();
+    font_cell.setColor(HSSFColor.BLACK.index);
+    font_cell.setFamily(FontFamily.MODERN);
+    font_cell.setFontName("Cambria");
+    stborder.setFont(font_cell);
+    stborder.setWrapText(true);
+    
+    
+          error_count =warning_count = 0; 
+    
+    
+        //add  headers to both
+        String headers[] = {"County","Sub County","Health Facility", "MFL Code","Calendar Year","Month","Year-Month","Program","Message","Age Group"};
+        int column_widths[] = {3500,5500,6500, 2500,2500,3500,2500,4500,18000,3500};
+        
+        XSSFRow rowError = sheetErrors.createRow(0);
+        XSSFRow rowWarning = sheetWarning.createRow(0);
+        XSSFCell cell_h_e,cell_h_w;  
+        for(int i=0;i<headers.length;i++){
+          cell_h_e = rowError.createCell(i);
+          cell_h_w = rowWarning.createCell(i);
+          
+          cell_h_e.setCellValue(headers[i]);
+          cell_h_w.setCellValue(headers[i]);
+          
+          cell_h_e.setCellStyle(stylex);
+          cell_h_w.setCellStyle(stylex);
+          
+          sheetErrors.setColumnWidth(i, column_widths[i]);
+          sheetWarning.setColumnWidth(i, column_widths[i]);
+         }
+        
+        //end of headers
+        
+        
+        if(obj.containsKey("output")){ // data from several facilities or several periods
+            JSONArray array_facil = (JSONArray)obj.get("output");
+            
+            for(int facils=0; facils<array_facil.size();facils++){
+                
+                
+             JSONObject facil_info = (JSONObject)array_facil.get(facils);
+             
+             facil_output(facil_info,sheetErrors,sheetWarning,stborder);
+             
+//           end of looping facilities  
+            }
+        }
+        else{ // for one facility, one period
+        facil_output(obj,sheetErrors,sheetWarning,stborder);    
+        }
+        
+        return wb;
+    }
+    
+    private void facil_output(JSONObject facil_info,XSSFSheet sheetErrors, XSSFSheet sheetWarning, XSSFCellStyle stborder){
+              
+        String month,mflcode,facility,subcounty,county,year,yearmonth;
+        int warnings,errors;
+        String age_group,message_type,program,message;
+        
+        
+             System.out.println("facil:"+facil_info);
+             month = facil_info.get("Month").toString();  
+             facility = facil_info.get("HealthFacility").toString();
+             subcounty = facil_info.get("SubCounty").toString();
+             county = facil_info.get("County").toString();
+             mflcode = facil_info.get("MFLCode").toString();
+             year = facil_info.get("Year").toString();
+             yearmonth = facil_info.get("yearmonth").toString();
+                      
+    
+             warnings = (Integer)facil_info.get("warnings");
+             errors = (Integer)facil_info.get("errors");
+        
+             JSONArray arr_details = (JSONArray) facil_info.get("details");
+             
+             //loop for errors
+             for(int details=0;details<arr_details.size();details++){
+                age_group = ((JSONObject)arr_details.get(details)).get("age_group").toString();
+                message_type = ((JSONObject)arr_details.get(details)).get("message_type").toString();
+                program = ((JSONObject)arr_details.get(details)).get("program").toString();
+                message = ((JSONObject)arr_details.get(details)).get("message").toString();
+
+                XSSFRow row_data;
+                //write data to excel based on error type
+                if(message_type.equalsIgnoreCase("error")){
+                  error_count++;
+                 row_data = sheetErrors.createRow(error_count);
+                }
+                else{
+                 warning_count++; 
+                 row_data = sheetWarning.createRow(warning_count);
+                }
+             
+                //add data to excel
+                String data[] = {county,subcounty,facility, mflcode,year,month,yearmonth,program,message,age_group};
+              
+                
+        XSSFCell cell_data;  
+        for(int i=0;i<data.length;i++){
+          cell_data = row_data.createCell(i);
+          if(isNumeric(data[i])){cell_data.setCellValue(Integer.parseInt(data[i]));}
+          else{cell_data.setCellValue(data[i]);}
+          cell_data.setCellStyle(stborder);
+         } 
+             }
+             
+               
+    }
+    
+    public boolean isNumeric(String s) {  
+        return s != null && s.matches("[-+]?\\d*\\.?\\d+");  
+}
 }
--- src/java/form1a/fas_trial.java
+++ src/java/form1a/fas_trial.java
@@ -5,46 +5,63 @@
  */
 package form1a;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.simple.JSONObject;
 
 /**
  *
  * @author GNyabuto
  */
 public class fas_trial extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    HttpSession session;
+    String facility_id,yearmonth;
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        try {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet fas_trial</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet fas_trial at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        } finally {
-            out.close();
-        }
+            throws ServletException, IOException, SQLException {
+        
+            JSONObject obj;
+            XSSFWorkbook wb;
+          ValidateExcel vExcel = new ValidateExcel();
+          
+          facility_id = "1";
+          yearmonth = "201902";
+          
+//          String facililtyids[]={"1","10","11","111","112","113","114","115","116","117","119","12","120","121","123","124","125","126","127","128","129","130","131","132","133","134","135","136","137","138","140","141","143","144","146","148","149","151","154","155","157","158","159","160","161","162","163","164","165","166","167","168","169","170","171","18","19","2","20","21","22","225","226","227","228","229","23","230","231","232","233","234","235","237","239","240","243","246","247","248","249","25","251","254","256","257","258","259","26","260","28","285","286","288","289","29","290","291","292","293","294","296","299","3","30","300","301","302","303","304","305","306","307","308","309","31","310","311","32","327","328","329","33","330","331","332","333","334","335","336","34","340","342","343","344","346","347","348","349","35","350","352","353","354","355","356","357","358","359","360","361","362","363","364","365","366","367","368","373","374","376","377","378","379","381","382","383","384","385","386","387","389","391","392","393","394","395","397","398","399","4","400","401","403","404","405","406","410","417","418","419","420","422","424","425","426","427","429","436","437","438","439","440","443","444","445","446","447","448","450","451","453","454","455","456","457","459","464","465","466","467","468","469","470","471","472","473","474","475","476","478","480","484","485","486","487","488","490","491","492","495","496","497","5","500","501","502","503","504","505","507","509","511","512","513","514","516","517","518","519","520","523","524","525","526","527","528","532","539","6","602","603","7","8","800","801","802","803","804","805","810","812","813","815","816","817","818","819","820","821","822","826","827","828","831","832","834","835","836","837","838","839","840","841","842","843","844","845","846","847","848","849","850","851"};
+//          String facililtyids[]={"1","10","11","111","112","113","114","115","116","117","119","12","120","121","123","124","125","126","127","128","129","130"};
+          String facililtyids[]={};
+//          String period[] = {"201810","201811","201812","201901","201902"};
+          String period[] = {"201902"};
+          
+          //call method to execute 
+//          obj = vExcel.validate(facility_id, yearmonth); // for one facility and one period
+          obj = vExcel.validate(facililtyids, period);
+          wb = vExcel.generateExcel(obj); // to generate Excel file
+//          out.println(obj);
+          
+    ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+    wb.write(outByteStream);
+    byte [] outArray = outByteStream.toByteArray();
+    response.setContentType("application/ms-excel");
+    response.setContentLength(outArray.length);
+    response.setHeader("Expires:", "0"); // eliminates browser caching
+    response.setHeader("Set-Cookie:", "fileDownload=true; path=/"); // set cookie header
+    response.setHeader("Content-Disposition", "attachment; filename=Data_Quality_Errors.xlsx");
+    OutputStream outStream = response.getOutputStream();
+    outStream.write(outArray);
+    outStream.flush();
+     
+      
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -59,7 +76,11 @@ public class fas_trial extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(fas_trial.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -73,7 +94,11 @@ public class fas_trial extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(fas_trial.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
